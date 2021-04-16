6c9c8a45b0a9eea1eecc0463d0f4fb118b827d41
Alexsandro Teixeira <alexsandro.tcunha@hotmail.com>
2021-04-16 10:33:00 -0300

    Criação do projeto e adição do model de produto.

---
+++ Fist-ReactJs/Fist-ReactJs/Configuration/Context.cs
@@ -0,0 +1,15 @@
+﻿using Fist_ReactJs.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fist_ReactJs.Configuration
+{
+    public class Context : DbContext
+    {
+        public DbSet<Produto> Produto { get; set; }
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated(); 
+        }
+    }
+}
---
+++ Fist-ReactJs/Fist-ReactJs/Model/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fist_ReactJs.Model
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("ID")]
+        public int ID { get; set; }
+        [Column("Descricao")]
+        public string Descricao { get; set; }
+    }
+}
